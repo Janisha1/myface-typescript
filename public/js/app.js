42300f560a166c2b2dcc5974e0de0d4fb2bda0b7
Janisha Patel <Janisha.Patel@softwire.com>
2023-07-20 16:04:54 +0100

    Almost there, just some small tweaks to do

--- public/js/app.js
+++ public/js/app.js
@@ -5,11 +5,21 @@ document.addEventListener("DOMContentLoaded", function () {
 function navMenu_Click(){
   const navBarMenu = document.getElementById("nav-header");
   const main = document.getElementById("mainScreenBlur");
+  //const openMenu = document.getElementById("open-nav-menu");
   if (navBarMenu.style.display === 'flex'){
     navBarMenu.style.display = 'none';
+ //   openMenu.style.display = 'fixed';
   /*  main.className = ""; */
   } else {
     navBarMenu.style.display = 'flex';
+ //   openMenu.style.display = 'none';
   /*  main.className = "addblur"; */
   }
+}
+
+function closeNavMenu_Click(){
+  const navBarMenu = document.getElementById("nav-header");
+ // const openMenu = document.getElementById("open-nav-menu");
+  navBarMenu.style.display = 'none';
+ // openMenu.style.display = 'fixed';
 }
